029656f1692651043ce8940d7bf9e7e5779e5d50
MrTofuuu <tofu@Tofu-PC.localdomain>
2022-07-29 01:06:15 -0500

    added typescript and javascript output folder

---
+++ javascript/index.js
@@ -0,0 +1,14 @@
+"use strict";
+require("dotenv").config();
+const { REST } = require("@discordjs/rest");
+const { Client, GatewayIntentBits, Routes } = require("discord.js");
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+try {
+    client.login(process.env.CLIENT_TOKEN);
+}
+catch (error) {
+    console.error("Error in client login");
+}
